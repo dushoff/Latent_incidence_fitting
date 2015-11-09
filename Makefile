c9cead4b04c72f23b50908a95c0e994f98969c15
Jonathan Dushoff <dushoff@mcmaster.ca>
2015-11-09 17:50:53 -0500

    Short and long-term forecasts in project.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 ### Hooks for the editor to set the default target
 current: target
 
-target pngtarget pdftarget vtarget acrtarget: NIH3.hybrid.Rout 
+target pngtarget pdftarget vtarget acrtarget: NIH3.project.Rout 
 
 ##################################################################
 
--- project.R
+++ project.R
@@ -1,6 +1,7 @@
 q <- c(0.05, 0.25, 0.5, 0.75, 0.95)
 qm <- (1+length(q))/2
-top <- 4
+topQuant <- 4
+shortWindow <- 4
 
 scenario <- sub("[.].*", "", rtargetname)
 
@@ -9,19 +10,26 @@ est <- t(apply(m, 2, function(v){quantile(v, probs = q)}))
 est <- est[grepl("obs", row.names(est)), ]
 print(est)
 
-t <- 1:nrow(est)
+forecast_plot <- function(finTime){
 
-plot(t, est[, qm]
-	, main=scenario
-	, type="l"
-	, xlab="Week", ylab="Cases"
-	, ylim = c(0, max(est[, top]))
-)
+	t <- 1:finTime
+	est <- est[1:finTime, ]
+	obs <- obs[1:finTime]
 
-points(t, obs)
-for(i in 1:length(q)){
-	lines(t, est[, i]
-		, lty = 1 + abs(i-qm)
+	plot(t, est[, qm]
+		, main=scenario
+		, type="l"
+		, xlab="Week", ylab="Cases"
+		, ylim = c(0, max(est[, topQuant]))
 	)
 
+	points(t, obs)
+	for(i in 1:length(q)){
+		lines(t, est[, i]
+			, lty = 1 + abs(i-qm)
+		)
+	}
 }
+
+forecast_plot(shortWindow+sum(!is.na(obs)))
+forecast_plot(nrow(est))
