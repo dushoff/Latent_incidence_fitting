19ef1966bce3d99eaaac4aaa65002474ed857a45
mikelin2 <wzmichael.li@gmail.com>
2015-11-19 23:15:48 -0500

    I think it works, i'll do stan tomorrow, good night

--- nimhybrid.R
+++ nimhybrid.R
@@ -12,30 +12,51 @@ effRepHb <- effRepHShape/effRepHmean
 preExp <- preMean/(lag+preMean)
 
 max <- length(obs)
-data <- list ("obs", "max", "lag", "lagvec", "pop"
-              , "foieps"
-              , "kappa"
-              , "effRepHa", "effRepHb"
-              , "preExp"
-              , "hetShape" , "hetMean"
-              , "shapeH"
-              , "Rshape", "Rmean"
-              , "gpShape", "gpMean"
-              , "gsShape", "gsMean"
-)
+data <- list (obs=obs)
 
 pre <- 1+obs[[1]]
 inits <- list(genPos = gpMean
               , effRep = maxRep
               , preInc = c(rep(pre, lag), 1+obs)
-              , predobs = c(100,100,100,100,100)
+#              , predobs = c(100,100,100,100,100)
+              , repShape=1
+              , incShape=1
+              , alpha=1
+              , RRprop=0.5
+              , R0=1
+              , genShape=1
+              , obsMean=obs
               )
 
+constants <- list(lag=lag
+                  ,foieps=foieps
+                  ,pop=pop
+                  ,kappa=kappa
+                  ,max=max
+                  ,effRepHa=effRepHa
+                  ,effRepHb=effRepHb 
+                  ,hetShape=hetShape
+                  ,hetMean=hetMean
+                  ,Rshape=Rshape 
+                  ,Rmean=Rmean
+                  ,gpShape=gpShape
+                  ,gpMean=gpMean
+                  ,gsShape=gsShape 
+                  ,gsMean=gsMean
+                  ,preExp=preExp
+                  ,shapeH=shapeH
+                  ,lagvec=lagvec)
 
+hybridMCMC <- MCMCsuite(code=nimcode,
+                        data=data,
+                        inits=inits,
+                        constants=constants,
+                        monitors=c('genPos'),
+                        niter=3000,
+                        MCMCs=c("jags","nimble"),
+                        makePlot=TRUE,
+                        savePlot=TRUE)
 
+mod <- nimbleModel(code=nimcode, data=data, inits=inits, constants=constants)
 
-inits <- list()
-
-constants <- list(lag=lag)
-
-hybridMCMC <- MCMCsuite(code)
+modmcmc <- buildMCMC(mod)
