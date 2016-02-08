4e6c2244ad768bf27d721816b83c818a86bebed0
Jonathan Dushoff <dushoff@mcmaster.ca>
2016-02-08 11:50:38 -0500

    Autocommit (Latent_incidence_fitting) On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   paramEsts.R
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	T3.NIH.incidence.csv
    	T3.NIH.peakWeek.csv
    	T4.NIH.incidence.csv
    	T4.NIH.params.csv
    	T4.NIH.peakWeek.csv
    	T5.NIH.incidence.csv
    	T5.NIH.paramEsts.csv
    	T5.NIH.params.csv
    	T5.NIH.peakWeek.csv
    	commit.time
    	curr

--- paramEsts.R
+++ paramEsts.R
@@ -5,6 +5,12 @@ df <- data.frame(
 	, 7*parest["gen", 3]
 	, 7*parest["gen", 2]
 	, 7*parest["gen", 4]
+	, NA*parest["R0", 3]
+	, NA*parest["R0", 2]
+	, NA*parest["R0", 4]
+	, parest["repMean", 3]
+	, parest["repMean", 2]
+	, parest["repMean", 4]
 )
 
 write.table(df, csvname, row.names=FALSE, col.names=FALSE, sep=",")
